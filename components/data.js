cae1109cee3875b9ff240841d21d32d877eb60f5
Agus <m.agustina.nahas@gmail.com>
2024-02-09 00:58:01 -0300

    first commit

---
+++ components/data.js
@@ -0,0 +1,224 @@
+export default [
+    {
+      "BARRIO": "Palermo",
+      "ESTRENAR": 626572,
+      "INDEX": 508160,
+      "USADO": 495893
+    },
+    {
+      "BARRIO": "Belgrano",
+      "ESTRENAR": 592840,
+      "INDEX": 497300,
+      "USADO": 482068
+    },
+    {
+      "BARRIO": "Chacarita",
+      "ESTRENAR": "-",
+      "INDEX": 492018,
+      "USADO": 473082
+    },
+    {
+      "BARRIO": "Nuñez",
+      "ESTRENAR": "-",
+      "INDEX": 486712,
+      "USADO": 475121
+    },
+    {
+      "BARRIO": "Retiro",
+      "ESTRENAR": 568544,
+      "INDEX": 466328,
+      "USADO": 448794
+    },
+    {
+      "BARRIO": "Recoleta",
+      "ESTRENAR": 578512,
+      "INDEX": 462257,
+      "USADO": 454989
+    },
+    {
+      "BARRIO": "Villa Urquiza",
+      "ESTRENAR": 509860,
+      "INDEX": 450036,
+      "USADO": 440567
+    },
+    {
+      "BARRIO": "Saavedra",
+      "ESTRENAR": "-",
+      "INDEX": 445375,
+      "USADO": 413330
+    },
+    {
+      "BARRIO": "Colegiales",
+      "ESTRENAR": 593485,
+      "INDEX": 437649,
+      "USADO": 418706
+    },
+    {
+      "BARRIO": "Coghlan",
+      "ESTRENAR": "-",
+      "INDEX": 437485,
+      "USADO": 427113
+    },
+    {
+      "BARRIO": "Villa Ortuzar",
+      "ESTRENAR": "-",
+      "INDEX": 414078,
+      "USADO": 412303
+    },
+    {
+      "BARRIO": "San Telmo",
+      "ESTRENAR": "-",
+      "INDEX": 406527,
+      "USADO": 389782
+    },
+    {
+      "BARRIO": "Villa Crespo",
+      "ESTRENAR": "-",
+      "INDEX": 406223,
+      "USADO": 393590
+    },
+    {
+      "BARRIO": "Monserrat",
+      "ESTRENAR": "-",
+      "INDEX": 399991,
+      "USADO": 392632
+    },
+    {
+      "BARRIO": "Paternal",
+      "ESTRENAR": "-",
+      "INDEX": 397500,
+      "USADO": "-"
+    },
+    {
+      "BARRIO": "Caballito",
+      "ESTRENAR": 471305,
+      "INDEX": 395763,
+      "USADO": 387158
+    },
+    {
+      "BARRIO": "Parque Chas",
+      "ESTRENAR": "-",
+      "INDEX": 395728,
+      "USADO": 387555
+    },
+    {
+      "BARRIO": "Almagro",
+      "ESTRENAR": 502045,
+      "INDEX": 394644,
+      "USADO": 378093
+    },
+    {
+      "BARRIO": "Villa Pueyrredon",
+      "ESTRENAR": "-",
+      "INDEX": 391978,
+      "USADO": 371357
+    },
+    {
+      "BARRIO": "Villa Devoto",
+      "ESTRENAR": "-",
+      "INDEX": 387405,
+      "USADO": 371684
+    },
+    {
+      "BARRIO": "Parque Chacabuco",
+      "ESTRENAR": "-",
+      "INDEX": 386018,
+      "USADO": 391430
+    },
+    {
+      "BARRIO": "Villa del Parque",
+      "ESTRENAR": "-",
+      "INDEX": 372662,
+      "USADO": 360404
+    },
+    {
+      "BARRIO": "Villa Gral. Mitre",
+      "ESTRENAR": "-",
+      "INDEX": 366101,
+      "USADO": 357671
+    },
+    {
+      "BARRIO": "San Nicolas",
+      "ESTRENAR": 441251,
+      "INDEX": 364883,
+      "USADO": 353427
+    },
+    {
+      "BARRIO": "Villa Luro",
+      "ESTRENAR": "-",
+      "INDEX": 361586,
+      "USADO": 319093
+    },
+    {
+      "BARRIO": "Flores",
+      "ESTRENAR": 411977,
+      "INDEX": 357820,
+      "USADO": 347326
+    },
+    {
+      "BARRIO": "Boedo",
+      "ESTRENAR": "-",
+      "INDEX": 357077,
+      "USADO": 355071
+    },
+    {
+      "BARRIO": "Balvanera",
+      "ESTRENAR": "-",
+      "INDEX": 352247,
+      "USADO": 347046
+    },
+    {
+      "BARRIO": "San Cristobal",
+      "ESTRENAR": "-",
+      "INDEX": 346679,
+      "USADO": 321082
+    },
+    {
+      "BARRIO": "Constitucion",
+      "ESTRENAR": "-",
+      "INDEX": 345833,
+      "USADO": 327082
+    },
+    {
+      "BARRIO": "Velez Sarsfield",
+      "ESTRENAR": "-",
+      "INDEX": 342225,
+      "USADO": "-"
+    },
+    {
+      "BARRIO": "Barracas",
+      "ESTRENAR": "-",
+      "INDEX": 329749,
+      "USADO": 326249
+    },
+    {
+      "BARRIO": "Villa Santa Rita",
+      "ESTRENAR": "-",
+      "INDEX": 318991,
+      "USADO": 316827
+    },
+    {
+      "BARRIO": "Floresta",
+      "ESTRENAR": "-",
+      "INDEX": 316520,
+      "USADO": 311154
+    },
+    {
+      "BARRIO": "Monte Castro",
+      "ESTRENAR": "-",
+      "INDEX": 314811,
+      "USADO": 315942
+    },
+    {
+      "BARRIO": "Mataderos",
+      "ESTRENAR": "-",
+      "INDEX": 310634,
+      "USADO": "-"
+    },
+    {
+      "BARRIO": "Liniers",
+      "ESTRENAR": "-",
+      "INDEX": 301392,
+      "USADO": 296495
+    }
+  ]
---
+++ components/map.js
@@ -0,0 +1,72 @@
+import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import { useEffect, useRef, useState } from 'react';
+// import './map.css' 
+import data from './data.js';
+import caba from './caba.js';
+
+export default function Map() {
+    mapboxgl.accessToken = process.env.TOKEN;
+
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-58.44504529638047);
+    const [lat, setLat] = useState(-34.61334039657031);
+    const [zoom, setZoom] = useState(11);
+
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+
+        console.log(data)
+        console.log(caba)
+
+        caba.features.map((f) => {
+            var final = f;
+            const barrio = f.properties.BARRIO
+            var properties = f.properties;
+            var mas = data.filter((d) => d.BARRIO.toLowerCase() === barrio.toLowerCase())[0]
+            final.properties = {...properties, ...mas}
+
+            return final;
+        })
+
+        console.log(caba)
+
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v12',
+            center: [lng, lat],
+            zoom: zoom
+        });
+
+        map.current.on('load', () => {
+            // Add a data source containing GeoJSON data.
+            map.current.addSource('maine', {
+                'type': 'geojson',
+                'data': caba
+            });
+             
+            // Add a new layer to visualize the polygon.
+            map.current.addLayer({
+                'id': 'maine',
+                'type': 'fill',
+                'source': 'maine', // reference the data source
+                'layout': {},
+                'paint': {
+                    'fill-color': {
+                        property: 'INDEX',
+                        stops: [[200000, '#ffff00'], [600000, '#ff0000']]
+                    },
+                    'fill-opacity': 0.5
+                }
+            });
+        });
+
+    }, []);
+
+    return (
+      <div>
+            <div ref={mapContainer} className="map-container" style={{ height: "75vh", width: "75vh" }} />
+      </div>
+    );
+  }
+  
