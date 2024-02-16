846b6bf9ebee958c7c5befeb6785079d370f8ba2
Agus <m.agustina.nahas@gmail.com>
2024-02-15 23:33:38 -0300

    estilos y mapa interactivo

--- components/map.js
+++ components/map.js
@@ -1,25 +1,21 @@
-import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import { useEffect, useRef, useState } from 'react';
-// import './map.css' 
 import data from './data.js';
 import caba from './caba.js';
 
-export default function Map() {
+import Map, {
+    Source,
+    Layer,
+  } from "react-map-gl";
+import { Slider, Typography } from '@mui/material';
 
-    mapboxgl.accessToken = process.env.NEXT_PUBLIC_TOKEN;
 
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lng, setLng] = useState(-58.44504529638047);
-    const [lat, setLat] = useState(-34.61334039657031);
-    const [zoom, setZoom] = useState(11);
+export default function CabaMap() {
+    
+    const [fullData, setFullData] = useState([]);
+    const [filtered, setFiltered] = useState([]);
+    const [sueldo, setSueldo] = useState(210000);
 
     useEffect(() => {
-        if (map.current) return; // initialize map only once
-
-        console.log(data)
-        console.log(caba)
-
         caba.features.map((f) => {
             var final = f;
             const barrio = f.properties.BARRIO
@@ -30,44 +26,73 @@ export default function Map() {
             return final;
         })
 
+        setFullData(caba)
+        // setFiltered(caba)
         console.log(caba)
-
-        map.current = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v12',
-            center: [lng, lat],
-            zoom: zoom
-        });
-
-        map.current.on('load', () => {
-            // Add a data source containing GeoJSON data.
-            map.current.addSource('maine', {
-                'type': 'geojson',
-                'data': caba
-            });
-             
-            // Add a new layer to visualize the polygon.
-            map.current.addLayer({
-                'id': 'maine',
-                'type': 'fill',
-                'source': 'maine', // reference the data source
-                'layout': {},
-                'paint': {
-                    'fill-color': {
-                        property: 'INDEX',
-                        stops: [[200000, '#ffff00'], [600000, '#ff0000']]
-                    },
-                    'fill-opacity': 0.5
-                }
-            });
-        });
 
     }, []);
 
+    useEffect(() => {
+        console.log(fullData)
+        console.log(fullData.features)
+        if (fullData && fullData.features) {
+
+            let fullCopy = JSON.parse(JSON.stringify(fullData.features))
+
+            var f = fullCopy.filter((barrio) => {
+                return barrio.properties['INDEX'] < sueldo
+            })
+
+            var dataFiltered = JSON.parse(JSON.stringify(fullData));
+            dataFiltered.features = f;
+    
+            setFiltered(dataFiltered)
+    
+        }
+
+    }, [fullData, sueldo])
+
+    const layerStyle = {
+        'type': 'fill',
+        'source-id': 'barrios',
+        'paint': {              
+            'fill-color': {
+                property: 'INDEX',
+                stops: [[200000, '#ffff00'], [600000, '#ff0000']]
+            },
+            'fill-opacity': 0.5
+        }
+      };
+
     return (
-      <div>
-            <div ref={mapContainer} className="map-container" style={{ height: "75vh", width: "75vh" }} />
+        <div style={{ padding: 50 }}>
+                <Typography component="h2">Ingresá tu sueldo</Typography>
+                <Typography component="h2">{sueldo}</Typography>
+
+                {(!filtered || !filtered.features) &&  <Typography component="h2">Ingresá tu sueldo</Typography>}
+                {filtered && filtered.features && filtered.features.length <= 0 && <Typography component="h2">Con ese presupuesto no hay deptos en alquiler en CABA</Typography>}
+                {filtered && filtered.features && filtered.features.length > 0 && filtered.features.length < 10 && <Typography component="h2">No pareciera que haya muchos barrios para alquilar con ese presupuesto</Typography>}
+                {filtered && filtered.features && filtered.features.length >= 10 && filtered.features.length < 30 && <Typography component="h2">Ese presupuesto nos da algunas opciones para alquilar</Typography>}
+                {filtered && filtered.features && filtered.features.length >= 30 && <Typography component="h2">Bue, el millonario</Typography>}
+                
+                <Slider min={100000} max={1000000} step={10000} defaultValue={210000} aria-label="Default" 
+                valueLabelDisplay="none" onChange={(a) => setSueldo(a.target.value) } />
+            {filtered && <Map
+                mapStyle="mapbox://styles/mapbox/streets-v9"
+                mapboxAccessToken={process.env.NEXT_PUBLIC_TOKEN}
+                boxZoom={false}
+                scrollZoom={false}
+                style={{
+                    height: '75vh',
+                    width: '80vw'
+                }}
+                initialViewState={{longitude: -58.44504529638047, latitude: -34.61334039657031, zoom: 11}}
+                >
+                    <Source id="barrios" type="geojson" data={filtered}>
+                        <Layer {...layerStyle}>
+                        </Layer>
+                    </Source>                
+            </Map>}
       </div>
     );
   }
-  
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import Map from "@/components/map";
+import CabaMap from "@/components/map";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <Map/>
+        <CabaMap/>
       </main>
     </>
   );
