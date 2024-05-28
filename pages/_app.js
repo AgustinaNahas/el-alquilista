6ac240aaf59d4d717144115a2e313dc5b28e2948
Agustina Nahas <achinahas95@gmail.com>
2024-05-28 17:15:59 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import "@/styles/globals.css";
 import { GoogleAnalytics } from "nextjs-google-analytics";
 
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+
 export default function App({ Component, pageProps }) {
+  console.log(process.env.NEXT_PUBLIC_GANALYTICS)
+  console.log(process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID)
+  console.log(GA_MEASUREMENT_ID)
+
   return <>
-    <GoogleAnalytics trackPageViews />
+    <GoogleAnalytics trackPageViews gaMeasurementId={GA_MEASUREMENT_ID}/>
     <Component {...pageProps} />
   </>;
 }
