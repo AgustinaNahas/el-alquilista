b8815c8d57080099c1b7f4cab8b2150c49b081b6
Agus <m.agustina.nahas@gmail.com>
2024-06-11 15:27:52 -0300

    console.log

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,9 @@ import { GoogleAnalytics } from '@next/third-parties/google'
 // const GA_MEASUREMENT_ID = "G-C3YJ4SEDJP";
 
 export default function App({ Component, pageProps }) {
+
+  console.log("app!")
+
   return <>
     <GoogleAnalytics gaId="G-C3YJ4SEDJP"/>
     <Component {...pageProps} />
